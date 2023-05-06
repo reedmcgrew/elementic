412fcb8bda1bd6051e0250e3b64737762ee844de
Reed McGrew <reedmcgrew@Reeds-MacBook-Air.local>
2023-05-06 17:16:53 -0400

    Refactors code to stop using globals outside of the 3 main game loop functions (init, draw, update).  Also groups code together in a more sensible way.

--- src/elementic.lua
+++ src/elementic.lua
@@ -1,5 +1,6 @@
--- constants
-local empty_tile = 0
+---
+--- Terrain Logic
+---
 local dirt = 11
 local dirt_slope_right = 13
 local dirt_slope_left = 12
@@ -27,6 +28,63 @@ local cloud = 44
 local map_width = 128
 local map_height = 16
 
+function is_damage_tile(tile)
+    return tile == magma
+end
+
+function is_solid_tile(tile)
+    return tile == stone or
+            tile == magma or
+            tile == grass or
+            tile == dirt or
+            tile == frozen_stone or
+            tile == snow or
+            tile == coolled_magma or
+            tile == metal or
+            tile == galixy or
+            tile == mossy_stone or
+            tile == corrupted_stone or
+            tile == corrupted_stone_wall or
+            tile == cloud
+end
+
+function is_right_slope_tile(tile)
+    return tile == stone_slope_right or
+            tile == magma_slope_right or
+            tile == grass_slope_right or
+            tile == dirt_slope_right
+end
+
+function is_left_slope_tile(tile)
+    return tile == stone_slope_left or
+            tile == magma_slope_left or
+            tile == grass_slope_left or
+            tile == dirt_slope_left
+end
+
+function get_slope_collision(tile, x)
+    local slope_height = 0
+    if tile == dirt_slope_right or tile == magma_slope_right or tile == grass_slope_right or tile == stone_slope_right then
+        slope_height = (x % 8)
+    elseif tile == dirt_slope_left or tile == magma_slope_left or tile == grass_slope_left or tile == stone_slope_left then
+        slope_height = 8 - (x % 8)
+    end
+    return slope_height
+end
+
+function get_slope_y(tile, x)
+    local slope_height = 0
+    if is_right_slope_tile(tile) then
+        slope_height = 0.5 * (x % 8)
+    elseif is_left_slope_tile(tile) then
+        slope_height = -0.5 * (x % 8) + 8
+    end
+    return slope_height
+end
+
+---
+--- Game State
+---
 local character = {
     x = 1*8,
     y = 7*8,
@@ -67,11 +125,172 @@ local boss = {
     speed = 0.5 -- movement speed
 }
 
-function is_damage_tile(tile)
-    return tile == magma
+---
+--- Character Logic
+---
+function update_character(character)
+    update_character_horizontal_movement(character)
+    update_character_vertical_movement(character)
 end
 
-function check_character_damage()
+function update_character_vertical_movement(character)
+    local new_y = character.y
+
+    -- Add jump input handling
+    if btnp(character.input.jump) and character.vy == 0 then
+        character.vy = character.jump_strength
+    end
+
+    -- Apply gravity
+    character.vy = character.vy + character.gravity
+    new_y = character.y + character.vy
+
+    -- Check for vertical collisions
+    local x_tile = flr((character.x + 4) / 8)
+    local y_tile_bottom = flr((new_y + 7) / 8)
+    local y_tile_top = flr((new_y + 6) / 8)
+    local tile_bottom = mget(x_tile, y_tile_bottom)
+    local tile_top = mget(x_tile, y_tile_top)
+
+    if is_solid_tile(tile_bottom) or is_solid_tile(tile_top) then
+        character.vy = 0
+    else
+        local slope_bottom_y = get_slope_y(tile_bottom, character.x + 4)
+        local slope_top_y = get_slope_y(tile_top, character.x + 4)
+
+        if slope_bottom_y > 0 or slope_top_y > 0 then
+            new_y = y_tile_bottom * 8 - slope_bottom_y - 7
+            character.vy = 0
+        end
+        character.y = new_y
+    end
+end
+
+function update_character_horizontal_movement(character)
+    local new_x = character.x
+
+    if btn(character.input.left) then
+        new_x = character.x - character.speed
+    end
+    if btn(character.input.right) then
+        new_x = character.x + character.speed
+    end
+
+    -- Check for horizontal collisions
+    local x_tile = flr((new_x + 4) / 8)
+    local y_tile = flr((character.y + 6) / 8)
+    local tile_left = mget(x_tile, y_tile)
+    local tile_right = mget(x_tile, y_tile)
+
+    if not (is_solid_tile(tile_left) or is_solid_tile(tile_right)) then
+        character.x = new_x
+    end
+end
+
+function handle_character_invulnerability(character)
+    if character.invulnerable then
+        character.invulnerable_timer = character.invulnerable_timer - 1
+        if character.invulnerable_timer <= 0 then
+            character.invulnerable = false
+            character.invulnerable_timer = 0
+        end
+    end
+end
+
+---
+--- Boss Logic
+---
+function update_boss(boss)
+    if boss.state == "holding_pattern" then
+        holding_pattern(boss)
+    elseif boss.state == "earth_shake" then
+        earth_shake(boss)
+    elseif boss.state == "vulnerability" then
+        handle_boss_invulnerability(boss)
+    end
+end
+
+function holding_pattern(boss)
+    -- calculate the horizontal movement range
+    local left_bound = boss_start_position_x
+    local right_bound = boss_start_position_x + boss.holding_pattern_width
+
+    -- move the boss horizontally
+    if boss.x >= right_bound then
+        boss.direction = -1
+    elseif boss.x <= left_bound then
+        boss.direction = 1
+    end
+
+    boss.x = boss.x + boss.speed * boss.direction
+
+    -- increment the earth_shake_timer and check if it's time to perform the earth shake move.
+    boss.earth_shake_timer = boss.earth_shake_timer + 1
+    if boss.earth_shake_timer >= 600 then -- 10 seconds * 60 frames per second
+        boss.earth_shake_timer = 0
+        boss.state = "earth_shake"
+    end
+end
+
+function earth_shake(boss)
+    -- move the boss down to the ground.
+    local target_y = map_height * 8 - 8
+    if boss.y < target_y then
+        boss.y = boss.y + 2
+    else
+        -- stay on the ground for 2 seconds.
+        boss.earth_shake_timer = boss.earth_shake_timer + 1
+        if boss.earth_shake_timer >= 120 then -- 2 seconds * 60 frames per second
+            boss.earth_shake_timer = 0
+            boss.y = boss_start_position_y
+            boss.state = "holding_pattern"
+        end
+    end
+end
+
+function handle_boss_invulnerability(boss)
+    -- change the boss's sprite to the frowny face.
+    boss.sprite = boss_impaired_sprite
+
+    -- increment the vulnerability_timer and check if the invulnerability period is over.
+    boss.vulnerability_timer = boss.vulnerability_timer + 1
+    if boss.vulnerability_timer >= 180 then -- 3 seconds * 60 frames per second
+        boss.vulnerability_timer = 0
+        boss.invulnerable = false
+        boss.sprite = boss_angry_sprite
+        boss.state = "holding_pattern"
+    end
+end
+
+---
+--- Character + Boss Interactions
+---
+function handle_boss_collision(character, boss)
+    if boss.invulnerable or boss.defeated then
+        return
+    end
+
+    local char_pixel_x = character.x + 5
+    local char_bottom_edge = character.y + 8
+
+    local boss_left_edge = boss.x
+    local boss_right_edge = boss.x + 7
+    local boss_top_edge = boss.y
+
+    if char_bottom_edge == boss_top_edge
+            and char_pixel_x >= boss_left_edge
+            and char_pixel_x <= boss_right_edge then
+        boss.health = boss.health - 1
+        boss.invulnerable = true
+        boss.state = "vulnerability"
+
+        if boss.health <= 0 then
+            boss.defeated = true
+        end
+    end
+end
+
+function check_character_damage(character, boss)
     -- Return if character is invulnerable
     if character.invulnerable then
         return
@@ -123,164 +342,45 @@ function check_character_damage()
     end
 end
 
-
-function character_collides_with_enemy()
-    -- check if the character collides with the boss's side or bottom
-    return not boss.defeated and
-            character.x + 8 >= boss.x and character.x <= boss.x + 7 and
-            character.y + 8 >= boss.y and character.y <= boss.y + 7
-end
-
--- create a function to draw the health meter
-function draw_health_meter()
-    local meter_width = 5 * character.max_health
-    local meter_height = 5
-    local meter_x = 127 * 8 - meter_width
-    local meter_y = 0
-
-    -- draw the background (empty health meter)
-    rectfill(meter_x, meter_y, meter_x + meter_width, meter_y + meter_height, 1)
-
-    -- draw the filled part of the health meter (remaining health)
-    local fill_width = meter_width * character.health / character.max_health
-    rectfill(meter_x, meter_y, meter_x + fill_width, meter_y + meter_height, 8)
-end
-
-function is_solid_tile(tile)
-    return tile == stone or
-            tile == magma or
-            tile == grass or
-            tile == dirt or
-            tile == frozen_stone or
-            tile == snow or
-            tile == coolled_magma or
-            tile == metal or
-            tile == galixy or
-            tile == mossy_stone or
-            tile == corrupted_stone or
-            tile == corrupted_stone_wall or
-            tile == cloud
-end
-
-function is_right_slope_tile(tile)
-    return tile == stone_slope_right or
-            tile == magma_slope_right or
-            tile == grass_slope_right or
-            tile == dirt_slope_right
-end
-
-function is_left_slope_tile(tile)
-    return tile == stone_slope_left or
-            tile == magma_slope_left or
-            tile == grass_slope_left or
-            tile == dirt_slope_left
-end
-
--- helper function to calculate y position on a slope
-function get_slope_collision(tile, x)
-    local slope_height = 0
-    if tile == dirt_slope_right or tile == magma_slope_right or tile == grass_slope_right or tile == stone_slope_right then
-        slope_height = (x % 8)
-    elseif tile == dirt_slope_left or tile == magma_slope_left or tile == grass_slope_left or tile == stone_slope_left then
-        slope_height = 8 - (x % 8)
-    end
-    return slope_height
-end
-
-function get_slope_y(tile, x)
-    local slope_height = 0
-    if is_right_slope_tile(tile) then
-        slope_height = 0.5 * (x % 8)
-    elseif is_left_slope_tile(tile) then
-        slope_height = -0.5 * (x % 8) + 8
-    end
-    return slope_height
-end
-
-function update_character_horizontal_movement()
-    local new_x = character.x
-
-    if btn(character.input.left) then
-        new_x = character.x - character.speed
-    end
-    if btn(character.input.right) then
-        new_x = character.x + character.speed
-    end
-
-    -- Check for horizontal collisions
-    local x_tile = flr((new_x + 4) / 8)
-    local y_tile = flr((character.y + 6) / 8)
-    local tile_left = mget(x_tile, y_tile)
-    local tile_right = mget(x_tile, y_tile)
-
-    if not (is_solid_tile(tile_left) or is_solid_tile(tile_right)) then
-        character.x = new_x
-    end
-end
-
-function update_character_vertical_movement()
-    local new_y = character.y
-
-    -- Add jump input handling
-    if btnp(character.input.jump) and character.vy == 0 then
-        character.vy = character.jump_strength
-    end
-
-    -- Apply gravity
-    character.vy = character.vy + character.gravity
-    new_y = character.y + character.vy
-
-    -- Check for vertical collisions
-    local x_tile = flr((character.x + 4) / 8)
-    local y_tile_bottom = flr((new_y + 7) / 8)
-    local y_tile_top = flr((new_y + 6) / 8)
-    local tile_bottom = mget(x_tile, y_tile_bottom)
-    local tile_top = mget(x_tile, y_tile_top)
-
-    if is_solid_tile(tile_bottom) or is_solid_tile(tile_top) then
-        character.vy = 0
-    else
-        local slope_bottom_y = get_slope_y(tile_bottom, character.x + 4)
-        local slope_top_y = get_slope_y(tile_top, character.x + 4)
-
-        if slope_bottom_y > 0 or slope_top_y > 0 then
-            new_y = y_tile_bottom * 8 - slope_bottom_y - 7
-            character.vy = 0
-        end
-        character.y = new_y
-    end
-end
-
--- Updated update_character function
-function update_character()
-    update_character_horizontal_movement()
-    update_character_vertical_movement()
-end
-
--- Add this function to your game loop (_update function)
-function update_invulnerability()
-    if character.invulnerable then
-        character.invulnerable_timer = character.invulnerable_timer - 1
-        if character.invulnerable_timer <= 0 then
-            character.invulnerable = false
-            character.invulnerable_timer = 0
-        end
-    end
-end
-
-function update_camera()
+---
+---  Main Drawing Logic
+---
+function update_camera(character)
     camera_x = character.x - 64
     camera_y = character.y - 64
 end
 
-function draw_character()
+function _draw()
+    cls()
+
+    -- Draw the map, character, and boss with the camera
+    camera(camera_x, camera_y)
+    map(0, 0, 0, 0, map_width, map_height)
+    draw_character(character)
+    draw_boss(boss)
+
+    -- Draw the health meter without camera adjustments
+    camera(0, 0)
+    draw_health_meter(character)
+
+    -- Draw debug info
+    draw_character_info(character)
+end
+
+function draw_boss(boss)
+    if not boss.defeated then
+        spr(boss.sprite, boss.x, boss.y)
+    end
+end
+
+function draw_character(character)
     -- Only draw the character if it's not invulnerable or blinking
     if not character.invulnerable or (character.invulnerable and (flr(time() * 8) % 2 == 0)) then
         spr(character.sprite, character.x, character.y, 1, 1)
     end
 end
 
-function draw_character_info()
+function draw_character_info(character)
     local info_x = 2
     local info_y = 10
 
@@ -294,104 +394,7 @@ function draw_character_info()
     print("Invulnerable: " .. tostring(character.invulnerable), info_x, info_y + 56, 7)
 end
 
-function holding_pattern(boss)
-    -- calculate the horizontal movement range
-    local left_bound = boss_start_position_x
-    local right_bound = boss_start_position_x + boss.holding_pattern_width
-
-    -- move the boss horizontally
-    if boss.x >= right_bound then
-        boss.direction = -1
-    elseif boss.x <= left_bound then
-        boss.direction = 1
-    end
-
-    boss.x = boss.x + boss.speed * boss.direction
-
-    -- increment the earth_shake_timer and check if it's time to perform the earth shake move.
-    boss.earth_shake_timer = boss.earth_shake_timer + 1
-    if boss.earth_shake_timer >= 600 then -- 10 seconds * 60 frames per second
-        boss.earth_shake_timer = 0
-        boss.state = "earth_shake"
-    end
-end
-
-function earth_shake(boss)
-    -- move the boss down to the ground.
-    local target_y = map_height * 8 - 8
-    if boss.y < target_y then
-        boss.y = boss.y + 2
-    else
-        -- stay on the ground for 2 seconds.
-        boss.earth_shake_timer = boss.earth_shake_timer + 1
-        if boss.earth_shake_timer >= 120 then -- 2 seconds * 60 frames per second
-            boss.earth_shake_timer = 0
-            boss.y = boss_start_position_y
-            boss.state = "holding_pattern"
-        end
-    end
-end
-
-function vulnerability(boss)
-    -- change the boss's sprite to the frowny face.
-    boss.sprite = boss_impaired_sprite
-
-    -- increment the vulnerability_timer and check if the invulnerability period is over.
-    boss.vulnerability_timer = boss.vulnerability_timer + 1
-    if boss.vulnerability_timer >= 180 then -- 3 seconds * 60 frames per second
-        boss.vulnerability_timer = 0
-        boss.invulnerable = false
-        boss.sprite = boss_angry_sprite
-        boss.state = "holding_pattern"
-    end
-end
-
--- update the check_boss_collision() function
-function check_boss_collision()
-    if boss.invulnerable or boss.defeated then
-        return
-    end
-
-    local char_pixel_x = character.x + 5
-    local char_bottom_edge = character.y + 8
-
-    local boss_left_edge = boss.x
-    local boss_right_edge = boss.x + 7
-    local boss_top_edge = boss.y
-
-    if char_bottom_edge == boss_top_edge
-            and char_pixel_x >= boss_left_edge
-            and char_pixel_x <= boss_right_edge then
-        boss.health = boss.health - 1
-        boss.invulnerable = true
-        boss.state = "vulnerability"
-
-        if boss.health <= 0 then
-            boss.defeated = true
-        end
-    end
-end
-
-
-function update_boss()
-    if boss.state == "holding_pattern" then
-        holding_pattern(boss)
-    elseif boss.state == "earth_shake" then
-        earth_shake(boss)
-    elseif boss.state == "vulnerability" then
-        vulnerability(boss)
-    end
-end
-
--- update the draw_boss() function
-function draw_boss()
-    if not boss.defeated then
-        spr(boss.sprite, boss.x, boss.y)
-    end
-end
-
--- Create a function to draw the health meter
-function draw_health_meter()
+function draw_health_meter(character)
     local meter_width = 5 * character.max_health
     local meter_height = 5
     local meter_x = (127 - character.max_health * 5)
@@ -405,32 +408,24 @@ function draw_health_meter()
     rectfill(meter_x, meter_y, meter_x + fill_width, meter_y + meter_height, 8)
 end
 
-
--- draw the map in the _draw() function
-function _draw()
-    cls()
-
-    -- Draw the map, character, and boss with the camera
-    camera(camera_x, camera_y)
-    map(0, 0, 0, 0, map_width, map_height)
-    draw_character()
-    draw_boss()
-
-    -- Draw the health meter without camera adjustments
-    camera(0, 0)
-    draw_character_info() -- Add this line
-    draw_health_meter()
-end
-
-
+---
+--- Main Game State Update Logic
+---
 function _update()
-    update_character()
-    update_camera()
-    update_boss()
-    check_boss_collision()
-    check_character_damage()
-    update_invulnerability()
+    -- Movement and Collision Detection
+    update_character(character)
+    update_camera(character)
+    update_boss(boss)
+
+    -- Damage Handling
+    handle_boss_collision(character, boss)
+    check_character_damage(character, boss)
+    handle_character_invulnerability(character)
 end
 
+
+---
+--- Initialize Game State
+---
 function _init()
 end
